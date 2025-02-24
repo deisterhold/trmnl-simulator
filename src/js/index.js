dfe82911d7cc40c220d24df0f92a36afe8b06f67
Daniel Eisterhold <deisterhold@gmail.com>
2025-02-24 17:39:39 -0600

    Fix async functions returning Promise.

--- src/js/index.js
+++ src/js/index.js
@@ -4,115 +4,121 @@ var frequencyInMs = 15 * 60 * 1000; // 15 mins
 
 async function setup(server) {
     if (!server) {
-        return Promise.reject(new Error('Server is not populated.'));
+        throw new Error('Server is not populated.');
     }
 
     // Grab Device ID
     const deviceId = document.getElementById('device-id').value || '';
 
     if (!deviceId) {
-        return Promise.reject(new Error('MAC Address is not populated.'));
+        throw new Error('MAC Address is not populated.');
     }
 
     // Grab API Key
     const apiKey = document.getElementById('api-key').value || '';
 
     if (apiKey) {
-        return Promise.resolve(apiKey);
+        return [deviceId, apiKey];
     }
 
-    try {
-        const url = new URL('/api/setup', server);
-        const resp = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'ID': deviceId
-            }
-        });
-
-        if (resp.status != 200) {
-            return Promise.reject(new Error(`HTTP Status Code (${resp.status}) does indicate success.`))
+    const url = new URL('/api/setup', server);
+    const resp = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'ID': deviceId
         }
+    });
 
-        const json = await resp.json();
-
-        console.log('Setup:', json);
-        if (json['status'] != 200) {
-            return Promise.reject(new Error(`API Status Code (${json['status']}) does indicate success.`));
-        }
-
-        document.getElementById('screen').src = json['image_url'];
-        document.getElementById('api-key').value = json['api_key'];
-        return Promise.resolve(json['api_key']);
-    } catch (err) {
-        return Promise.reject(err);
+    if (resp.status != 200) {
+        throw new Error(`HTTP Status Code (${resp.status}) does indicate success.`);
     }
+
+    const json = await resp.json();
+
+    console.log('Setup:', json);
+    if (json['status'] != 200) {
+        let message;
+
+        if (json && json['message']) {
+            message = json['message'].toString();
+        } else {
+            message = `API Status Code (${json['status']}) does indicate success.`
+        }
+
+        throw new Error(message);
+    }
+
+    // Set screen to initial value and populate API field
+    document.getElementById('screen').src = json['image_url'];
+    document.getElementById('api-key').value = json['api_key'];
+
+    return [deviceId, json['api_key']];
 }
 
 async function display(server, deviceId, key) {
     if (!server) {
-        return Promise.reject(new Error('Server is not populated.'));
+        throw new Error('Server is not populated.');
     }
 
     if (!deviceId) {
-        return Promise.reject(new Error('MAC Address is not populated.'));
+        throw new Error('MAC Address is not populated.');
     }
 
     if (!key) {
-        return Promise.reject(new Error('API Key is not populated.'));
+        throw new Error('API Key is not populated.');
     }
 
-    try {
-        const url = new URL('/api/display', server);
-        const resp = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'ID': deviceId,
-                'Access-Token': key,
-                'Refresh-Rate': (frequencyInMs / 1000).toFixed()
-            }
-        });
-
-        if (resp.status != 200) {
-            return Promise.reject(new Error(`HTTP Status Code (${resp.status}) does indicate success.`))
+    const url = new URL('/api/display', server);
+    const resp = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'ID': deviceId,
+            'Access-Token': key,
+            'Refresh-Rate': (frequencyInMs / 1000).toFixed()
         }
+    });
 
-        const json = await resp.json();
-
-        console.log('Display:', json);
-        if (json['status'] != 0) {
-            return Promise.reject(new Error(`API Status Code (${json['status']}) does indicate success.`));
-        }
-
-        document.getElementById('screen').src = json['image_url'];
-        const frequency = parseInt(json['refresh_rate'], 10);
-        if (!isNaN(frequency)) {
-            console.log(`Setting refresh frequency to ${frequency} seconds.`);
-            frequencyInMs = frequency * 1000;
-        }
-
-        return Promise.resolve();
-    } catch (err) {
-        Promise.reject(err);
+    if (resp.status != 200) {
+        throw new Error(`HTTP Status Code (${resp.status}) does indicate success.`);
     }
+
+    const json = await resp.json();
+
+    console.log('Display:', json);
+    if (json['status'] != 0) {
+        let message;
+
+        if (json && json['error']) {
+            message = json['error'].toString();
+        } else {
+            message = `API Status Code (${json['status']}) does indicate success.`
+        }
+
+        throw new Error(message);
+    }
+
+    document.getElementById('screen').src = json['image_url'];
+    const frequency = parseInt(json['refresh_rate'], 10);
+    if (!isNaN(frequency)) {
+        console.log(`Setting refresh frequency to ${frequency} seconds.`);
+        frequencyInMs = frequency * 1000;
+    }
+
+    return;
 }
 
-async function loop(server, deviceId) {
-    try {
-        const apiKey = await setup(server);
-        
-        await display(server, deviceId, apiKey);
+async function loop(server) {
+    const [deviceId, apiKey] = await setup(server);
 
-        timeoutId = 0;
-        start(false);
-    } catch (err) {
-        return Promise.reject(err);
-    }
+    await display(server, deviceId, apiKey);
+
+    timeoutId = 0;
+    start(false);
 }
 
 function stop() {
     if (!timeoutId) return;
-    
+
     console.log('Stopping...');
     // Cancel any running timeout
     clearTimeout(timeoutId);
@@ -155,7 +161,7 @@ function start(firstRun = true) {
 }
 
 // Check for query params on page load
-window.onload = function() {
+window.onload = function () {
     var parameters = new URLSearchParams(location.search);
 
     const serverInput = document.getElementById('server');
